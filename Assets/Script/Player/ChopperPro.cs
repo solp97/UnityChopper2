68c339509bf634672b41a6e87dff07705e3a4ffe
Jaeyeon <zeeoe@naver.com>
2022-07-09 17:57:01 +0900

    V1

--- Assets/Script/Player/ChopperPro.cs
+++ Assets/Script/Player/ChopperPro.cs
@@ -9,6 +9,7 @@ public class ChopperPro : MonoBehaviour
     public GameObject Chopper;
     public float speed = 0f;
     public float fly = 0f;
+    public float RotateSpeed = 0f;
     void Update()
     {
         if (Input.GetKey(KeyCode.W))
@@ -22,20 +23,32 @@ public class ChopperPro : MonoBehaviour
             if(speed >= 15f)
             {
                 speed = 15f;
-                fly += 0.0008f;
+                fly += 0.008f;
 
                     Chopper.transform.position = Chopper.transform.position + new Vector3(0, fly, 0);
             }
         }
         else
         {
-            speed -= 0.015f;
-            fly -= 0.00001f;
+            speed -= 0.01f;
             Propeller.transform.Rotate(0f, speed, 0f);
             Propeller2.transform.Rotate(0f, speed, 0f);
             if (speed <= 0f) speed = 0f;
             if (fly <= 0f) fly = 0f;
-            Chopper.transform.position = Chopper.transform.position + new Vector3(0, fly, 0);
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            fly -= 0.0005f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            RotateSpeed = -0.5f;
+            Chopper.transform.Rotate(0f, RotateSpeed, 0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            RotateSpeed = 0.5f;
+            Chopper.transform.Rotate(0f, RotateSpeed,0f);
         }
     }
 }
