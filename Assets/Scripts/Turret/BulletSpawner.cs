f19948f2d9bb469ae90eedde114c03393e0bb238
Jaeyeon <zeeoe@naver.com>
2022-07-15 10:31:00 +0900

    Idiot Turret

--- Assets/Scripts/Turret/BulletSpawner.cs
+++ Assets/Scripts/Turret/BulletSpawner.cs
@@ -11,6 +11,9 @@ public class BulletSpawner : MonoBehaviour
     public float randomNumber;
     public float randomMin = 1f;
     public float randomMax = 3f;
+    public float nowAngle;
+    public float maxAngle;
+    public float minAngle;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,23 +23,35 @@ public class BulletSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        float angle = Quaternion.Angle(transform.rotation, Target.rotation);
         timer += Time.deltaTime;
-        transform.Rotate(0f, RotateSpeed, 0f);
+        //transform.Rotate(0f, RotateSpeed, 0f);
 
+        Vector3 distanceVector = Target.position - transform.position;
+        nowAngle = Mathf.Atan2(distanceVector.x, distanceVector.z) * Mathf.Rad2Deg;
+        Debug.Log(Vector3.Dot(transform.forward, distanceVector.normalized));
+        Debug.Log($"Áö±Ý °¢{nowAngle}");
+        Debug.DrawRay(transform.position, transform.forward * 5, Color.magenta);
+        Debug.DrawRay(transform.position, distanceVector, Color.red);
     }
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.tag =="Player")
+        Vector3 distanceVector = Target.position - transform.position;
+        if (other.gameObject.tag =="Player" && nowAngle >= maxAngle && nowAngle <= minAngle )
         {
             transform.LookAt(Target);
-            if (timer >= randomNumber)
-            {
-                Instantiate(BulletPrefab,transform.position, transform.rotation);
-                timer = 0;
-                randomNumber = Random.Range(randomMin, randomMax);
-            }
+            fire();
         }
 
+    }
+
+    private void fire()
+    {
+        if (timer >= randomNumber)
+        {
+            Instantiate(BulletPrefab, transform.position, transform.rotation);
+            timer = 0;
+            randomNumber = Random.Range(randomMin, randomMax);
+        }
     }
 }
